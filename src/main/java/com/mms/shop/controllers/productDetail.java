cbae384587554d3a96fb523f082a9ac3e2569e19
Mar Mar Soe <marmarsoe@isgm2.com>
2020-08-24 16:52:11 +0630

    update detail page

--- src/main/java/com/mms/shop/controllers/productDetail.java
+++ src/main/java/com/mms/shop/controllers/productDetail.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class productDetail {
 	
-	@RequestMapping("/productDetail")
+	@RequestMapping("/detail")
 	public String getProductDetail() {
 		return "product_detail";
 	}
